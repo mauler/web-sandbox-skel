e011ad385faac0335b85815d238df08cc737e2bf
Paulo R <proberto.macedo@gmail.com>
2017-04-11 13:11:04 -0300

    Added static urls to project

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls.static import static
 from django.conf.urls import include, url
+from django.conf import settings
 
 from rest_framework.documentation import include_docs_urls
 
@@ -6,4 +8,4 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     url(r'^docs/', include_docs_urls(title='PROJECT')),
     url(r"^v1/members/", include("member.urls", namespace="member")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
