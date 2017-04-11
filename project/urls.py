cf4f14de8816797e406478d99b1c51ac4726ddcb
Paulo R <proberto.macedo@gmail.com>
2017-04-11 12:14:31 -0300

    Added docs support

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 
+from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
-    url("^v1/members/", include("member.urls", namespace="member")),
+    url(r'^docs/', include_docs_urls(title='PROJECT')),
+    url(r"^v1/members/", include("member.urls", namespace="member")),
 ]
